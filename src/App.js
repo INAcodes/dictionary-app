e88ea76e9e64355a95c457602f6650a750e0c98b
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-02-24 15:54:34 +0100

    added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-       
-      </header>
+      <header className="App-header">Dictionary App</header>
+        <main>
+        <Dictionary />
+        </main>
+      <footer className="text-center">
+        Coded by <em>INA HADAJ</em>
+      </footer>
     </div>
   );
 }
