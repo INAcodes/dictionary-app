9755978033897477b57c74842b165a5b09875b5e
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-02-24 17:51:41 +0100

    added results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,15 @@
 import React,{useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary(){
     let [keyword, setKeyword]= useState("");
+    let [results, setResults]= useState({});
 
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+
 
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         //documentatio:https://dictionaryapi.dev/
@@ -15,17 +17,19 @@ export default function Dictionary(){
     }
 
     function handleResponse(response){
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
     }
 
-    return (<div className="Dictionary">
+    return (
+    <div className="Dictionary">
        <form onSubmit={search}>
         <input type="search"  onChange={handleKeywordChange} />
        </form>
+       <Results results={results} />
     </div>
     );
 }
