e10745c93a84d16d843e2d47600b501ac7549061
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-02-25 15:04:35 +0100

    added section in dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,12 +6,17 @@ import Results from "./Results.js";
 export default function Dictionary(){
     let [keyword, setKeyword]= useState("");
     let [results, setResults]= useState(null);
+    let [loaded, setLoaded]= useState(false);
 
-    function search(event){
+
+    function handleSubmit(event){
         event.preventDefault();
+        search();
+    }
 
 
-        let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    function search(){
+       let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         //documentatio:https://dictionaryapi.dev/
         axios.get(apiUrl).then(handleResponse);
     }
@@ -24,12 +29,28 @@ export default function Dictionary(){
         setKeyword(event.target.value);
     }
 
+    function load(){
+      setLoaded(true);
+      search();
+    }
+     
+    if (loaded){
     return (
     <div className="Dictionary">
-       <form onSubmit={search}>
-        <input type="search"  onChange={handleKeywordChange} />
-       </form>
+     <section>
+       <h1>What word do you want to look up?</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="search"  onChange={handleKeywordChange} />
+        </form>
+       <div className="hint">
+         suggested words: sunset, wine, yoga, plant...
+        </div>
+      </section>
        <Results results={results} />
     </div>
     );
+     } else { 
+        load();
+      return "Loading";
+  }
 }
